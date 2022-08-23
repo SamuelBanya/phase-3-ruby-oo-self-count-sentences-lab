9c4db40e3dceae2c98e4fd2ba72611371531dd90
Samuel Banya <sbanya@fastmail.com>
2022-08-23 18:49:16 -0400

    Adding latest changes to assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
+# NOTE: 'monkey patching': the practice of adding methods to or altering Ruby's built-in classes
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # This example is ridiculous since no where on the actual lesson is regex taught
+    # for something like this, and chaining isn't even mentioned:
+    count = self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    puts count
+    count
   end
 end
