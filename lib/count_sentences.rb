bc3f09f701bdc8d90dee57dfb5534a9d1c40c60f
Samuel Banya <sbanya@fastmail.com>
2022-08-23 18:43:52 -0400

    commit tree 6b3e4bbd43171d087e20b88a9f96758a4f83079b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
+# NOTE: 'monkey patching': the practice of adding methods to or altering Ruby's built-in classes
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    puts count
+    count
   end
 end
